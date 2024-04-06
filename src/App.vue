52075010dcbff7f7fa21e5c20407438695d95224
MikiZenebe <mickyaszenebe@gmail.com>
2024-04-05 22:15:38 -0500

    ui

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 card p-20 rounded-lg shadow-2xl">
+  <div class="flex flex-col gap-2 card p-12 rounded-lg shadow-2xl h-auto">
     <Header />
     <Balance />
     <IncomeExpenses />
     <TransactionList />
+    <AddTransaction />
   </div>
 </template>
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <h3 class="text-md font-semibold text-white pt-3 border-b-2">
+      Add Transaction
+    </h3>
+    <div class="flex flex-col gap-1">
+      <div>
+        <input
+          class="w-full transition-all duration-[300ms] ease-out rounded-md appearance-none border border-gray-300 py-0.5 px-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-800 focus:border-transparent placeholder:text-sm"
+          type="text"
+          placeholder="Enter Reason"
+        />
+      </div>
+      <div>
+        <input
+          class="w-full transition-all duration-[300ms] ease-out rounded-md appearance-none border border-gray-300 py-0.5 px-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-green-800 focus:border-transparent placeholder:text-sm"
+          type="text"
+          placeholder="Amount (-ve-expense,+ve-expense)"
+        />
+      </div>
+
+      <button
+        class="bg-green-600 mt-6 p-1.5 text-white rounded-md hover:bg-white hover:text-green-500 transition ease-in"
+      >
+        Add Transaction
+      </button>
+    </div>
+  </div>
+</template>
